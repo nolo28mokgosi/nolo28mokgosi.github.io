b8f470120467917bd3f480ed089c00f838c5d681
nolomokgosi <nolomokgosi@gmail.com>
2021-11-17 11:37:25 +0000

    Fixing custom domain

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/nolomokgosi/Development/git-source-repo/nolomokgosi-blog/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/nolomokgosi/Development/git-source-repo/nolomokgosi-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/nolomokgosi/Development/git-source-repo/nolomokgosi-blog/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/nolomokgosi/Development/git-source-repo/nolomokgosi-blog/src/css/custom.css"),
+];
