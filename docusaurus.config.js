11fa2dc81e0e69103ae4407e39927805b775aca1
nolomokgosi <nolomokgosi@gmail.com>
2021-11-18 13:53:49 +0000

    cname on config v2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,6 @@ const config = {
    organizationName: 'nolo28mokgosi', // Usually your GitHub org/user name.
    projectName: 'nolo28mokgosi.github.io', // Usually your repo name.
    deploymentBranch: 'gh-pages', //specify since my repo name has .github.io
-   cname: 'nolomokgosi.com',
 
   presets: [
     [
